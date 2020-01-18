a4a366652cf20e5f23a032d8c1eea8926da7c2eb
Lukáš Kurčík <lukas.kurcik@gmail.com>
2020-01-18 12:14:45 +0100

    Automatic snapping to grid

---
+++ Assets/Scripts/SnapToGrid.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnapToGrid : MonoBehaviour
+{
+#if UNITY_EDITOR
+    public bool snapToGrid = true;
+    public float snapValue = 1.0f;
+ 
+    public bool sizeToGrid = false;
+    public float sizeValue = 1.0f;
+#endif
+}
