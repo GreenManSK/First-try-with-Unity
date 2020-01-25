24cf6f653e6ba548c2f5dc67d8368e4e59165235
Lukáš Kurčík <lukas.kurcik@gmail.com>
2020-01-25 10:44:31 +0100

    Color constants

---
+++ Assets/Scripts/Constants/Colors.cs
@@ -0,0 +1,27 @@
+using System.Linq.Expressions;
+using UnityEngine;
+
+namespace Constants
+{
+    public class Colors
+    {
+        public static readonly Color Orange = GetColor("#d47564");
+        public static readonly Color Yellow = GetColor("#e8c498");
+        public static readonly Color White = GetColor("#ecece0");
+        public static readonly Color Turquoise = GetColor("#4fa4a5");
+        public static readonly Color Green = GetColor("#aad395");
+        public static readonly Color Black = GetColor("#3b324a");
+        public static readonly Color Blue = GetColor("#5c6182");
+        
+        private Colors()
+        {
+        }
+
+        private static Color GetColor(string hex)
+        {
+            Color color;
+            ColorUtility.TryParseHtmlString(hex, out color);
+            return color;
+        }
+    }
+}
