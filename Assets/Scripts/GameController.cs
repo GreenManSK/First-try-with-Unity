8906a0aa5f5a79d59fef56fe8aa31a33d5f1aed0
Lukáš Kurčík <lukas.kurcik@gmail.com>
2020-01-25 12:23:34 +0100

    Global volume level lowered

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        AudioListener.volume = .5f;
+    }
+}
