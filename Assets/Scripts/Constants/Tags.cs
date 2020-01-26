403b9f9183e14a72ac4d0e3d4117d1410a1ae51c
Lukáš Kurčík <lukas.kurcik@gmail.com>
2020-01-26 18:40:34 +0100

    Drop item object

--- Assets/Scripts/Constants/Tags.cs
+++ Assets/Scripts/Constants/Tags.cs
@@ -4,6 +4,7 @@ namespace Constants
     {
         public const string Destroyable = "Destroyable";
         public const string Player = "Player";
+        public const string Drop = "Drop";
 
         private Tags()
         {
---
+++ Assets/Scripts/Drops/DropItemController.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+public class DropItemController : MonoBehaviour
+{
+    public DropType type = DropType.Unknown;
+    public int qunatity = 1;
+}
