2a134996543eb2db9853ed155bb93802e1deb825
Lukáš Kurčík <lukas.kurcik@gmail.com>
2020-01-28 17:25:04 +0100

    Player inventory

---
+++ Assets/Scripts/InventoryController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class InventoryController : MonoBehaviour
+{
+    private Dictionary<DropType, int> inventory;
+
+    private void Start()
+    {
+        var types = Enum.GetValues(typeof(DropType)).Cast<DropType>();
+        foreach (var type in types)
+        {
+            inventory.Add(type, 0);
+        }
+    }
+
+    public int Get(DropType type)
+    {
+        return inventory[type];
+    }
+    
+    public void Add(DropType type, int volume)
+    {
+        inventory[type] += volume;
+    }
+
+    public bool Remove(DropType type, int volume)
+    {
+        if (inventory[type] < volume)
+            return false;
+        inventory[type] -= volume;
+        return true;
+    }
+}
