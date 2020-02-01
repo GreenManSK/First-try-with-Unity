3582429cda74d28d87be6e875d956fe5b878594e
Lukáš Kurčík <lukas.kurcik@gmail.com>
2020-02-01 16:26:11 +0100

    Lower base sound

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -7,7 +7,7 @@ public class GameController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        AudioListener.volume = .5f;
+        AudioListener.volume = .1f;
     }
     
     void OnGUI()
